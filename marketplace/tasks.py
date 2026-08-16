5e28fe7f38a2141b97e23b37819e7f54a7644614
Saravanan <saravananshavan852.gmail.com>
2026-08-16 22:07:59 +0530

    Upgrade verification email to premium HTML template and update subject line

--- marketplace/tasks.py
+++ marketplace/tasks.py
@@ -5,16 +5,28 @@ from django.conf import settings
 @shared_task
 def send_verification_email_async(user_id, token_id, domain, scheme):
     from .models import CustomUser, EmailVerificationToken
+    from django.template.loader import render_to_string
+    from django.utils.html import strip_tags
     try:
         user = CustomUser.objects.get(pk=user_id)
         token = EmailVerificationToken.objects.get(pk=token_id)
         verification_url = f'{scheme}://{domain}/verify-email/{token.token}/'
+        
+        context = {
+            'user': user,
+            'verification_url': verification_url
+        }
+        
+        html_message = render_to_string('emails/verification_email.html', context)
+        plain_message = strip_tags(html_message)
+        
         send_mail(
-            subject='Verify your email - சந்தை பேரங்காடி',
-            message=f'Hi {user.username},\n\nClick the link below to verify your email:\n{verification_url}\n\nIf you did not register, please ignore this email. This link will expire in 24 hours.',
+            subject='Verify your email - PocketSanthai',
+            message=plain_message,
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[user.email],
             fail_silently=False,
+            html_message=html_message
         )
     except Exception as e:
         import logging
